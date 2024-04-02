93f8c34df212530d86f8519f9f9a880168d8ff1f
Mher Grigoryan <mger_grigoryan@mail.ru>
2024-04-03 01:53:07 +0400

    Trial version 1

---
+++ script.js
@@ -0,0 +1,15 @@
+// JavaScript для анимации появления блоков при прокрутке
+window.addEventListener('scroll', () => {
+    const blocks = document.querySelectorAll('.container');
+    blocks.forEach(block => {
+        if (window.innerHeight > block.getBoundingClientRect().top + 100) {
+            block.classList.add('show');
+        }
+    });
+});
+
+// JavaScript для анимации появления кнопки при загрузке страницы
+window.addEventListener('load', () => {
+    const btn = document.querySelector('.btn');
+    btn.classList.add('show');
+});
